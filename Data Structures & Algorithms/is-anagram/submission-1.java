a7d0ff7fc072c2756141a0bb952cbc00098d9746
Deeptadeep Roy <deeptadeeproy@yahoo.in>
2026-08-24 16:27:38 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+    
+        char[] charArrS = s.toCharArray();
+        char[] charArrT = t.toCharArray();
+        
+        Arrays.sort(charArrS);
+        Arrays.sort(charArrT);
+
+        return Arrays.equals(charArrS, charArrT);
+    }
+}
